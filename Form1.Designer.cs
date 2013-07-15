e9665d9ee58b662211cd18bb4af18d92123f6f5c
Marcus Classon <marcus.g.classon@gmail.com>
2013-07-15 13:49:37 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MemoryLeak
+{
+    partial class HostWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.memoryLabel = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Allocate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AllocateObjects);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Invoke";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.TriggerChildObjects);
+            // 
+            // memoryLabel
+            // 
+            this.memoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.memoryLabel.Location = new System.Drawing.Point(29, 169);
+            this.memoryLabel.Name = "memoryLabel";
+            this.memoryLabel.Size = new System.Drawing.Size(535, 53);
+            this.memoryLabel.TabIndex = 2;
+            // 
+            // countLabel
+            // 
+            this.countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countLabel.Location = new System.Drawing.Point(29, 222);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(535, 53);
+            this.countLabel.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(385, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Dispose";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Dispose);
+            // 
+            // HostWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 305);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.memoryLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "HostWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label memoryLabel;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryLeak
+{
+    public partial class HostWindow : Form
+    {
+        public event EventHandler ev_Click;
+        public HostWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AllocateObjects(object sender, EventArgs e)
+        {
+            ((Button)sender).Enabled = false;
+            for (int a = 0; a < 10000; a++)
+            {
+                new Worker(this);
+            }
+            memoryLabel.Text = "Memory consumption:" + GC.GetTotalMemory(true)/1024;
+            
+
+        }
+
+        private void TriggerChildObjects(object sender, EventArgs e)
+        {
+            countLabel.Text = "InvocationList contains " + (ev_Click == null?0:ev_Click.GetInvocationList().Length) + " objects";
+        }
+
+        private void Dispose(object sender, EventArgs e)
+        {
+            if (ev_Click == null)
+                return;
+            foreach (var w in ev_Click.GetInvocationList())
+            {
+                using (var x = w.Target as IDisposable)
+                {
+                    x.Dispose();
+                }
+            }
+            countLabel.Text = "InvocationList contains " + (ev_Click == null ? 0 : ev_Click.GetInvocationList().Length) + " objects";
+            memoryLabel.Text = "Memory consumption:" + GC.GetTotalMemory(true) / 1024;
+
+        }
+
+    }
+    class Worker : IDisposable
+    {
+        public delegate void Click(object sender);
+        private readonly byte[] bLoad = new byte[99999];
+        HostWindow _host;
+        public Worker(HostWindow host)
+        {
+            _host = host;
+            host.ev_Click += HostEventTriggered;
+        }
+
+        void HostEventTriggered(object sender, EventArgs args)
+        {
+            Console.WriteLine("I'm triggered");
+        }
+
+
+        public void Dispose()
+        {
+            _host.ev_Click -= HostEventTriggered;
+        }
+    }
+}
